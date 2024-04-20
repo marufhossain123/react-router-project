fb6ed10af6a0ac93f2cb3b0744430c3ca727793b
sharif <srka780@gmail.com>
2024-04-21 00:54:23 +0600

    help with us

--- src/postDetails/PostDetails.jsx
+++ src/postDetails/PostDetails.jsx
@@ -1,18 +1,19 @@
-import { useLoaderData } from "react-router-dom";
+import { Link, useLoaderData } from "react-router-dom";
 
 const PostDetails = () => {
-    const post = useLoaderData();
-    console.log(post)
-    const {id,title,body} = post;
-    return (
-        <div>
-            <h3>post details about:{id}</h3>
-            <p>title:{title}</p>
-            <p><small>body:{body}</small></p>
-            <Link to={`/post/${id}`}>post details</Link>
-
-        </div>
-    );
+  const post = useLoaderData();
+  console.log(post);
+  const { id, title, body } = post;
+  return (
+    <div>
+      <h3>post details about:{id}</h3>
+      <p>title:{title}</p>
+      <p>
+        <small>body:{body}</small>
+      </p>
+      <Link to={`/post/${id}`}>post details</Link>
+    </div>
+  );
 };
 
 export default PostDetails;
